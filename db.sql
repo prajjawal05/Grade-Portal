6f538ac6cf9328aac336d61887592c7b24746a08
Prajjawal Agarwal <prajjawalagarwal@gmail.com>
2017-09-07 18:57:28 +0530

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,8 @@
+create database library;
+use library;
+create table grade (Id varchar(40),marks varchar(40) );
+create table StudLogin ( Id varchar(40), pwd varchar(40) );
+create table InviLogin ( Id varchar(40), pwd varchar(40) );
+insert into StudLogin values("iit2015099","nice");
+insert into InviLogin values("haha","nice");
+
